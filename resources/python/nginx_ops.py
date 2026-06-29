9fa70fa0276ff5ecc6439317e6c7e9cd81dec8c6
eduardoftmoreira-citeve <emoreira@citeve.pt>
2026-06-29 18:29:30 +0100

    fix: nginx_ops will now deal with proxy .conf files through direct access to the reverse proxy

--- resources/python/nginx_ops.py
+++ resources/python/nginx_ops.py
@@ -27,10 +27,10 @@ class NginxOps:
         with open(conf_name, 'w') as f:
             f.write(config_content)
         
-        locations_dir = Configuration.get_nginx_locations()
         nginx_container = Configuration.get_nginx_container()
         
-        run_command(f"mv {conf_name} {locations_dir}/", check=True)
+        run_command(f"docker cp {conf_name} {nginx_container}:/etc/nginx/locations/{conf_name}", check=True)
+        run_command(f"rm {conf_name}", check=False)
         
         test_result = subprocess.run(
             f"docker exec {nginx_container} nginx -t 2>/dev/null",
@@ -41,16 +41,16 @@ class NginxOps:
             run_command(f"docker exec {nginx_container} nginx -s reload", check=True)
             print(f"{Configuration.get_log_success()} Proxy configured: {base_path}")
         else:
-            run_command(f"rm {locations_dir}/{conf_name}", check=False)
+            run_command(f"docker exec {nginx_container} rm -f /etc/nginx/locations/{conf_name}", check=False)
             print(f"{Configuration.get_log_fail()} Invalid Nginx configuration")
             raise Exception("Invalid Nginx configuration")
     
     def remove_config(self, project, branch):
         """Remove Nginx proxy configuration"""
         conf_name = f"{Configuration.get_pilot_tag()}-{project.name}-{branch}.conf"
-        locations_dir = Configuration.get_nginx_locations()
+        nginx_container = Configuration.get_nginx_container()
         
-        run_command(f"rm -f {locations_dir}/{conf_name}", check=False)
+        run_command(f"docker exec {nginx_container} rm -f /etc/nginx/locations/{conf_name}", check=False)
         print(f"{Configuration.get_log_info()} Removed config: {conf_name}")
     
     def reload(self):
